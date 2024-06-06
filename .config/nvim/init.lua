65a9ee48a485a9ebe6f8561cf7eb5f1df07edec7
Matthew Mourgos <mgmourgos@users.noreply.github.com>
2024-06-06 02:25:40 -0700

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,132 @@
-require("mm")
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.opt.scrolloff = 8 
+
+vim.opt.colorcolumn = "80"
+
+vim.g.mapleader = " "
+
+-- Sync clipboard between OS and Neovim.
+-- See `:h 'clipboard'`
+vim.opt.clipboard = 'unnamedplus'
+
+vim.keymap.set("n", "<leader>p", vim.cmd.Ex)
+
+vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv")
+
+-- Moving pg up/down or forward/backward in search will keep cursor centered.
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("i", "kj", "<Esc>")
+vim.keymap.set("i", "jk", "<Esc>")
+
+-- Format the entire file
+--vim.keymap.set("n", "<leader>q", "gggqG<C-o>")
+vim.keymap.set("n", "<leader>q", "<cmd>:lua vim.lsp.buf.format()<CR>")
+
+-- Clear hightlight on search by pressing <Esc> in normal mode. 
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+--  {"catppuccin/nvim",
+--    lazy = false,
+--    priority = 1000,
+--    init = function ()
+--      --vim.cmd.colorscheme "catppuccin-mocha"
+--    end,
+--  },
+  "ThePrimeagen/vim-be-good",
+
+  "neovim/nvim-lspconfig",
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = { "cpp", "c", "lua", "vim", "vimdoc", "javascript", "html" },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+    end
+ }, 
+
+  "rebelot/kanagawa.nvim",
+}, opts)
+
+local on_attach = function(client, bufnr)
+  local function buf_set_option(...)
+    vim.api.nvim_buf_set_option(bufnr, ...)
+  end
+
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Disable the lsp semantic hightlighting which was sometimes overwriting the
+  -- initial hightlighting done by treesitter.
+  client.server_capabilities.semanticTokensProvider = nil
+
+  -- Mappings.
+  local opts = { buffer = bufnr, noremap = true, silent = true }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, opts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+  vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+  vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+  -- vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+  -- 'gq{motion}' is how to auto format TODO format whole file?
+end
+
+  require'lspconfig'.clangd.setup{
+  on_attach = on_attach
+  }
+
+vim.cmd.colorscheme "kanagawa"
